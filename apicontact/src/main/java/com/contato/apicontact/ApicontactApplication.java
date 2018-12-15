115012fa0f3bffe04f7fec179042d5be52ac0efe
Lucas Moura <lmoura@thoughtworks.com>
2018-12-15 11:10:13 -0200

    NONSTORY [~lmoura] Create base api

---
+++ apicontact/src/main/java/com/contato/apicontact/ApicontactApplication.java
@@ -0,0 +1,14 @@
+package com.contato.apicontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicontactApplication.class, args);
+	}
+
+}
+
