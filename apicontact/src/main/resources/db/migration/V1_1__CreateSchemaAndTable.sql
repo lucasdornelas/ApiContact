46c914a90cb01275f4d5d6d04dd56a288c7cb7e8
Lucas Moura <lmoura@thoughtworks.com>
2018-12-16 09:10:49 -0200

    NONSTORY [~lmoura] Created a new project

--- apicontact/src/main/resources/db/migration/V1_1__CreateSchemaAndTable.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE api_contact_schema.player (
-  id                  SERIAL PRIMARY KEY,
-  name                VARCHAR
-);
-
-CREATE SEQUENCE contact_register_id_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
